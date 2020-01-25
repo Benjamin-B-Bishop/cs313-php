110f74008f4f574b5481dd35b720d57a7ac8b2a8
Benjamin <bis11012@byui.edu>
2020-01-25 16:46:09 -0700

    test

--- web/ShoppingCart/browse.php
+++ web/ShoppingCart/browse.php
@@ -12,7 +12,11 @@
         <form method="post" action="view.php">
             <p>Please select from the following:</p><br><br><br>
 
-            <input type="checkbox" name="addedItems[]" value="chair" onclick="addToCart('chair', '20')">
+            <label>
+                <input type="checkbox" name="items[]" value="Cup-Noodles" >
+                <i></i> <img src="images/Cup-Noodles.jpg" alt="Cup-Noodles" style="width:250px;height:250px;">
+            </label>
+
  
             <input type="submit" name="submit" value="View cart">
         </form>
