5240ab85aa484368f87a878216fccf0a319d1273
Benjamin <bis11012@byui.edu>
2020-01-25 16:32:31 -0700

    first commit

---
+++ web/ShoppingCart/browse.php
@@ -0,0 +1,20 @@
+  
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Browse</title>
+        
+        <script>
+            function addToCart(itemName, price) {
+
+            }
+        </script>
+    </head>
+    <body>
+        <form method="post" action="view.php">
+            <p>Please select from the following:</p><br><br><br>
+ 
+            <input type="submit" name="submit" value="View cart">
+        </form>
+    </body>
+</html>
