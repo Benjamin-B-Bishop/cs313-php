e990881af996925f552820eb116ef690b4bc0ef2
Benjamin <bis11012@byui.edu>
2020-01-25 16:54:53 -0700

    images

--- web/ShoppingCart/browse.php
+++ web/ShoppingCart/browse.php
@@ -14,12 +14,12 @@
 
             <label>
                 <input type="checkbox" name="items[]" value="Cup_Noodles" >
-                <i></i> <img src="images/Cup_Noodles.jpg" alt="Cup_Noodles" >
+                <i></i> <img src="images/Cup_Noodles.jpg" alt="Cup_Noodles" style="width:634px;height:748px;">
             </label>
 
             <label>
                 <input type="checkbox" name="items[]" value="Portal_Device" >
-                <i></i> <img src="images/Portal_Device.jpg" alt="Portal_Device" >
+                <i></i> <img src="images/Portal_Device.jpg" alt="Portal_Device" style="width:512px;height:307px;">
             </label>
  
             <input type="submit" name="submit" value="View cart">
