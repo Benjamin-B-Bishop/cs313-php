223ba5c40042b8541b37207b2364b46d6b4f1d10
Benjamin <bis11012@byui.edu>
2020-01-25 16:38:00 -0700

    test

--- web/ShoppingCart/browse.php
+++ web/ShoppingCart/browse.php
@@ -1,9 +1,7 @@
-  
-<!DOCTYPE html>
+ <!DOCTYPE html>
 <html>
     <head>
-        <title>Browse</title>
-        
+        <title>Browse</title>        
         <script>
             function addToCart(itemName, price) {
 
@@ -13,6 +11,8 @@
     <body>
         <form method="post" action="view.php">
             <p>Please select from the following:</p><br><br><br>
+
+            <input type="checkbox" name="addedItems[]" value="chair" onclick="addToCart('chair', '20')">
  
             <input type="submit" name="submit" value="View cart">
         </form>
