a3ae7068a9cd1f468196092f724ffda0f1b29230
Benjamin <bis11012@byui.edu>
2020-02-10 16:49:56 -0700

    week 6 reading help

--- web/week05/scripturesView.php
+++ web/week05/scripturesView.php
@@ -1,19 +1,16 @@
 <?php
-require "connections.php";
+require ('connections.php');
 $db = get_db();
 
-$query = 'SELECT id, book FROM scripture');
+$query = 'SELECT id, book FROM scripture';
 $stmt  = $db->prepare($query);
 $stmt -> execute();
-$scriptures = $stmt-fetchALL(PDO::FETCH_ASSOC);
+$scriptures = $stmt->fetchALL(PDO::FETCH_ASSOC);
 
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<html>
+<head>>
     <title>Scripture Resources</title>
 </head>
 <body>
@@ -25,13 +22,14 @@ $scriptures = $stmt-fetchALL(PDO::FETCH_ASSOC);
 <?php
 foreach ($scriptures as $scripture)
 {
-  $id   = $scripture['id'];
-  $book = $scripture['book'];
-  echo "<li><p>$book<p></li>";
+    $id   = $scripture['id'];
+    $book = $scripture['book'];
+
+    echo "<li><p> $book </p></li>";
 }  
 ?>
 
-</ul>
+</ul> 
 
 </body>
 </html>
