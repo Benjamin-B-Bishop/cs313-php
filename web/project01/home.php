71bc5c3ad71ac80248324ac0f8717a2672132d6b
Benjamin <bis11012@byui.edu>
2020-02-15 17:32:10 -0700

    project01 update

---
+++ web/project01/home.php
@@ -0,0 +1,33 @@
+<?php
+require "C:\Users\BaldBen\cs313-php\web\week05\connections.php";
+$db = get_db();
+
+$stmt  = $db->prepare("SELECT id, book FROM scripture") ;
+$stmt->execute();
+$scriptures = $stmt->fetchALL(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Scripture Resources</title>
+</head>
+<body>
+
+<h1>Scripture Resources</h1>
+
+<ul>
+
+<?php
+foreach ($scriptures as $scripture)
+{
+    $book = $scripture['book'];
+
+    echo "<li><p> $book </p></li>";
+}  
+?>
+
+</ul> 
+
+</body>
+</html>
