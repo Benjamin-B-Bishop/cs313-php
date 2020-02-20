44e2a4496e673d79c238d3341fba348abc796e61
Benjamin <bis11012@byui.edu>
2020-02-19 17:14:23 -0700

    project 1

--- web/project01/home.php
+++ web/project01/home.php
@@ -1,10 +1,10 @@
 <?php
-// require "C:\Users\BaldBen\cs313-php\web\week05\connections.php";
-// $db = get_db();
+require "C:\Users\BaldBen\cs313-php\web\week05\connections.php";
+$db = get_db();
 
-// $stmt  = $db->prepare("SELECT id, c_name FROM player") ;
-// $stmt->execute();
-// $scriptures = $stmt->fetchALL(PDO::FETCH_ASSOC);
+$stmt  = $db->prepare("SELECT id, c_name FROM player") ;
+$stmt->execute();
+$players = $stmt->fetchALL(PDO::FETCH_ASSOC);
 
 ?>
 <!DOCTYPE html>
@@ -25,12 +25,12 @@
 <ul>
 
 <?php
-// foreach ($scriptures as $scripture)
-// {
-//     $book = $scripture['book'];
+foreach ($players as $player)
+{
+    $c_name = $player['c_name'];
 
-//     echo "<li><p> $book </p></li>";
-// }  
+    echo "<li><p> $c_name </p></li>";
+}  
 ?>
 
 </ul> 
